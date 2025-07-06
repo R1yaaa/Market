09d512a6429e4ed78726023736dad613f3e408ad
Your Name <you@example.com>
2025-07-06 15:40:57 +0200

    qt Änderungen, endpunkte doc

--- extra/requests_client.py
+++ extra/requests_client.py
@@ -6,20 +6,41 @@ from PySide6 import QtWidgets
 from ui_form import Ui_Widget
 import sys
 
+#BASE = "http://localhost:8000"
+
+#pyside6-uic form.ui -o ui_form.py um py Datei zu erstellen
+
+#sudo apt update
+#sudo apt install libxcb-cursor0 libxcb-xinerama0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1
+
+
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
 
-        # Beispiel: Nach dem Start Login-Seite anzeigen
+        # Nach dem Start Login-Seite anzeigen
         self.ui.stackedWidget.setCurrentWidget(self.ui.pageLogin)
 
         # Login verbinden
         self.ui.pushButtonLogin.clicked.connect(self.login)
 
+        #Register verbinden
+        self.ui.pushButtonRegister.clicked.connect(self.register)
+
         # Logout verbinden
         self.ui.pushButtonLogout.clicked.connect(self.logout)
+
+        #Inventar verbinden
+        self.ui.pushButtonInventory.clicked.connect(self.inventory)
+
+        #Zurück-Button
+        self.ui.pushButtonZurueck.clicked.connect(self.back)
+
+
+
+
 
         # Tabelle konfigurieren
         #self.setup_markttabelle()
@@ -27,13 +48,32 @@ class MainWindow(QWidget):
     def login(self):
         name = self.ui.lineEditUsername.text()
         if name:
-            self.ui.labelCoins.setText("Coins: 100.00")
+            self.ui.labelCoins.setText("100.00")
             self.ui.stackedWidget.setCurrentWidget(self.ui.pageMarket)
         else:
             self.ui.labelStatus.setText("Bitte Namen eingeben")
 
+
+
+    def register(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.pageMarket)
+
+
+
     def logout(self):
         self.ui.stackedWidget.setCurrentWidget(self.ui.pageLogin)
+
+
+    def inventory(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.pageInventar)
+
+    def back(self):
+        self.ui.stackedWidget.setCurrentWidget(self.ui.pageMarket)
+
+
+    #def setup_inventartabelle(self):
+
+    #def setup_offertabelle(self):
 
     def setup_markttabelle(self):
         table = self.ui.tableMarkt
@@ -61,6 +101,9 @@ class MainWindow(QWidget):
         print(f"Gekauft: {name}")
         # später REST-Request senden
 
+
+
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
@@ -68,5 +111,5 @@ if __name__ == "__main__":
     sys.exit(app.exec())
 
 
-#BASE = "http://localhost:8000"
+
 
